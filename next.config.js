92d87e2509324a0aac5a2aa9e5ea6a3059d5cacd
Pitta <shuna120700@gmail.com>
2026-01-15 16:56:14 +0800

    fix: パスエイリアスを使用してインポートパスを修正

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@/components': path.resolve(__dirname, 'presentation/web/components'),
+      '@/hooks': path.resolve(__dirname, 'presentation/web/hooks'),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
